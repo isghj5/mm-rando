eadc50fd8bce968a34d71401f266433bebee2674
isghj5 <isghj5@gmail.com>
2020-06-14 23:53:54 -0700

    Unique music slots: Spiderhouse no longer uses don gero slot

--- MMR.Randomizer/Enemies.cs
+++ MMR.Randomizer/Enemies.cs
@@ -360,8 +360,8 @@ namespace MMR.Randomizer
 
             }.ToList();
 
-            var WholeList = WeakEnemiesOnly ? WeakEnemyList : WeakEnemyList.Concat(AnnoyingEnemyList);
-            //var WholeList = WeakEnemyList;
+            //var WholeList = WeakEnemiesOnly ? WeakEnemyList : WeakEnemyList.Concat(AnnoyingEnemyList);
+            var WholeList = WeakEnemyList;
 
             foreach (GameObjects.Actor enemy in WholeList)
             {
