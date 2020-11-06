90891969778c9fd18106bf05a7188ebc4ef00123
isghj5 <isghj5@gmail.com>
2020-11-06 15:01:17 -0800

    backported fix for bug from 1.13

--- MMR.Randomizer/Builder.cs
+++ MMR.Randomizer/Builder.cs
@@ -158,7 +158,7 @@ namespace MMR.Randomizer
                 {
                     SequenceInfo testSeq = Unassigned[i];
                     // increases chance of getting non-mm music, but only if we have lots of music remaining
-                    if (Unassigned.Count > 77 && testSeq.Name.StartsWith("mm") && (random.Next(100) < 40))
+                    if (Unassigned.Count > 77 && testSeq.Name.StartsWith("mm") && testSeq.Type[0] < 0x100 && (random.Next(100) < 40))
                         continue;
 
                     // test if the testSeq can be used with available instrument set slots
