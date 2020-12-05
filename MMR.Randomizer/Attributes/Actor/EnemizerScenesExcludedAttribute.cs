f109719b15f71498c9ee2d75e1c470f5a3df9aff
isghj5 <isghj5@gmail.com>
2020-12-04 18:01:35 -0800

    Enemizer WIP rework 3

---
+++ MMR.Randomizer/Attributes/Actor/EnemizerScenesExcludedAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MMR.Randomizer.Attributes.Actor
+{
+    /// <summary>
+    /// this blocks the enemy from being randomized in a certain scene
+    ///   example: we dont want to randomize the octorocks that are used for freezing with ice arrows in ikana
+    /// </summary>
+
+    class EnemizerScenesExcludedAttribute : Attribute
+    {
+       public List<int> ScenesExcluded { get; private set; }
+
+        public EnemizerScenesExcludedAttribute(int scene, params int[] additionalScenes)
+        {
+            var scenes = new List<int> { scene };
+            if (additionalScenes.Length > 0)
+            {
+                scenes.AddRange(additionalScenes);
+            }
+            ScenesExcluded = scenes;
+        }
+
+    }
+}
---
+++ MMR.Randomizer/Attributes/Actor/RespawningVarientsAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMR.Randomizer.Attributes.Actor
+{
+    /// <summary>
+    /// some enemies have varities that can respawn, and some "kill all enemies to continue" rooms get confused by respawning enemies
+    /// </summary>
+
+    class RespawningVarientsAttribute : Attribute
+    {
+        public List<int> Varients { get; private set; }
+
+        public RespawningVarientsAttribute(int varient, params int[] additionalVarients)
+        {
+            var v = new List<int> { varient };
+            if (additionalVarients.Length > 0)
+            {
+                v.AddRange(additionalVarients);
+            }
+            Varients = v;
+        }
+    }
+}
