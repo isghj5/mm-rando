0d00d1ee7954dcdd2d047e5a606c1d561803712c
isghj5 <isghj5@gmail.com>
2020-12-09 00:18:59 -0800

    Enemizer WIP rework 5

--- MMR.Randomizer/Extensions/ActorExtensions.cs
+++ MMR.Randomizer/Extensions/ActorExtensions.cs
@@ -3,6 +3,11 @@ using MMR.Randomizer.GameObjects;
 using MMR.Randomizer.Attributes.Actor;
 using MMR.Common.Extensions;
 using MMR.Randomizer.Utils;
+using System.Runtime.InteropServices;
+using System;
+using System.Runtime.CompilerServices;
+using System.CodeDom;
+using System.Linq;
 
 namespace MMR.Randomizer.Extensions
 {
@@ -23,11 +28,6 @@ namespace MMR.Randomizer.Extensions
             return actor.GetAttribute<EnemizerEnabledAttribute>()?.Enabled ?? false;
         }
 
-        public static ActorType EnemyType(this Actor actor)
-        {
-            return actor.GetAttribute<EnemyTypeAttribute>().Type;
-        }
-
         public static List<int> RespawningVariants(this Actor actor)
         {
             return actor.GetAttribute<RespawningVarientsAttribute>()?.Variants;
@@ -35,12 +35,29 @@ namespace MMR.Randomizer.Extensions
 
         public static List<int> Variants(this Actor actor)
         {
-            return actor.GetAttribute<ActorVariantsAttribute>()?.Variants;
+            var variants = new List<int>();
+            var attrF = actor.GetAttribute<FlyingVariantsAttribute>();
+            if (attrF != null)
+            {
+                variants.Union(attrF.Variants);
+            }
+            var attrW = actor.GetAttribute<WaterVariantsAttribute>();
+            if (attrW != null)
+            {
+                variants.Union(attrW.Variants);
+            }
+            var attrG = actor.GetAttribute<GroundVariantsAttribute>();
+            if (attrG != null)
+            {
+                variants.Union(attrG.Variants);
+            }
+
+            return variants;
         }
 
-        public static List<int> NonRespawningVariants(this Actor actor)
+        public static List<int> NonRespawningVariants(this Actor actor, List<int> acceptableVariants = null)
         {
-            var nonRespawningVariants = Variants(actor);
+            var nonRespawningVariants = acceptableVariants != null ? acceptableVariants : Variants(actor);
             var respawningVariants    = RespawningVariants(actor);
             if (respawningVariants != null && respawningVariants.Count > 0)
             {
@@ -64,9 +81,56 @@ namespace MMR.Randomizer.Extensions
                 Object       = actor.ObjectIndex(),
                 ObjectSize   = ObjUtils.GetObjSize(actor.ObjectIndex()),
                 Variables    = actor.Variants(),
-                Type         = (int) actor.EnemyType(),
                 SceneExclude = actor.ScenesRandomizationExcluded()
             };
+        }
+
+        public static List<int> CompatibleVariants(this Actor actor, Actor otherActor, Random rng)
+        {
+            // with mixed types, typing could be messy, keep it hidden here
+            // EG. like like can spawn on the sand on the surface, but also on the bottom of GBC
+
+            // I'm sure theres a cleaner way, but everything I tried C# said no
+            var listOfVariants = new List<byte>() {1 , 2 , 3 };
+            listOfVariants.ForEach(u => rng.Next()); // random sort in case it has multiple types
+            foreach( var variant in listOfVariants)
+            {
+                List<int> compatibleVariants = null;
+                if (variant == 1) // water
+                {
+                    var ourAttr = actor.GetAttribute < WaterVariantsAttribute >();
+                    var theirAttr = otherActor.GetAttribute< WaterVariantsAttribute >();
+                    if (ourAttr != null && theirAttr != null)
+                    {
+                        compatibleVariants = (ourAttr.Variants).FindAll(u => theirAttr.Variants.Contains(u));
+                    }
+                }
+                if (variant == 2) // ground
+                {
+                    var ourAttr = actor.GetAttribute<GroundVariantsAttribute>();
+                    var theirAttr = otherActor.GetAttribute<GroundVariantsAttribute>();
+                    if (ourAttr != null && theirAttr != null)
+                    {
+                        compatibleVariants = (ourAttr.Variants).FindAll(u => theirAttr.Variants.Contains(u));
+                    }
+                }
+                if (variant == 3) // flying
+                {
+                    var ourAttr = actor.GetAttribute<FlyingVariantsAttribute>();
+                    var theirAttr = otherActor.GetAttribute<FlyingVariantsAttribute>();
+                    if (ourAttr != null && theirAttr != null)
+                    {
+                        compatibleVariants = (ourAttr.Variants).FindAll(u => theirAttr.Variants.Contains(u));
+                    }
+                }
+
+                if (compatibleVariants != null && compatibleVariants.Count > 0)
+                {
+                    return compatibleVariants;
+                }
+            }
+
+            return null;
         }
     }
 }
