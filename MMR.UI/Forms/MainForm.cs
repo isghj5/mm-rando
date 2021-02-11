174ad5c2053d93c417786f089dcd31502fb9e906
isghj5 <isghj5@gmail.com>
2021-02-11 11:33:12 -0800

    Enemizer core changes: better actor size detection, catching free and companion actors, but not counting new actors if previously existed but unchanged

--- MMR.UI/Forms/MainForm.cs
+++ MMR.UI/Forms/MainForm.cs
@@ -68,7 +68,7 @@ namespace MMR.UI.Forms
             this.KeyDown += MainForm_KeyDown_CtrlS;
 
 
-            Text = $"Majora's Mask Randomizer v{Randomizer.AssemblyVersion} + Isghj's Enemizer Test 13.1";
+            Text = $"Majora's Mask Randomizer v{Randomizer.AssemblyVersion} + Isghj's Enemizer Test 13.2";
         }
 
         private void InitializeTooltips()
