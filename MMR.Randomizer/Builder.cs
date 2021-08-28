d44b698ba6567ead53906018e425d78670384a36
isghj8 <isghj8@gmail.com>
2021-08-28 10:40:22 -0700

    No music: cleaned the music muting code for consistency with the rest of MMRs code style

--- MMR.Randomizer/Builder.cs
+++ MMR.Randomizer/Builder.cs
@@ -97,7 +97,6 @@ namespace MMR.Randomizer
                 SequenceUtils.WriteSongLog(log, settings);
             }
         }
-        #endregion
 
         private void WriteAudioSeq(Random random, OutputSettings _settings)
         {
@@ -127,19 +126,27 @@ namespace MMR.Randomizer
         {
             if (_cosmeticSettings.Music == Music.None)
             {
+                /// mute all music by setting their master volume to zero
                 // Traverse the audioseq index table to get the locations of all sequences
-                byte[] audioseq_table = RomData.MMFileList[RomUtils.GetFileIndexForWriting(Addresses.SeqTable)].Data;
-                // turns out the randomizer doesn't consider the table to be its own file, we need the offset
-                int audioseq_table_baseaddr = RomData.MMFileList[RomUtils.GetFileIndexForWriting(Addresses.SeqTable)].Addr;
-                byte[] audioseq = RomData.MMFileList[RomUtils.GetFileIndexForWriting(0x00046AF0)].Data; // 46AF0 is audioseq starting location
+                // the audioseq index table is not its own file, its buried within the code file, we need the offset to the table
+                var codeFile = RomData.MMFileList[RomUtils.GetFileIndexForWriting(Addresses.SeqTable)];
+                var audioseqIndexTable = codeFile.Data;
+                int audioseqIndexTableAddr = Addresses.SeqTable - codeFile.Addr;
+                var audioseq = RomData.MMFileList[RomUtils.GetFileIndexForWriting(Addresses.AudioSequence)].Data;
                 // for each sequence, search for the master volume byte and change to zero
-                for (int seq = 2; seq < 128; seq += 1){
+                for (int seq = 2; seq < 128; seq += 1)
+                {
                     if (seq == 0x54) // It was requested that the bar band minigame not be silenced
+                    {
                         continue;
-                    int seq_location_offset = (int)ReadWriteUtils.Arr_ReadU32(audioseq_table, (Addresses.SeqTable + seq * 16) - audioseq_table_baseaddr);
-                    for (int byte_iter = 3; byte_iter < 128; byte_iter++){
-                        if (audioseq[seq_location_offset + byte_iter] == 0xDB){
-                            audioseq[seq_location_offset + byte_iter + 1] = 0x0;
+                    }
+
+                    int seqLocation = (int)ReadWriteUtils.Arr_ReadU32(audioseqIndexTable, audioseqIndexTableAddr + (seq * 16));
+                    for (int b = 3; b < 128; b++) // search for master volume byte
+                    {
+                        if (audioseq[seqLocation + b] == 0xDB) // master volume byte
+                        { 
+                            audioseq[seqLocation + b + 1] = 0x0; // set value to zero
                             continue;
                         }
                     }
@@ -156,6 +163,7 @@ namespace MMR.Randomizer
 
             Enemies.DisableEnemyCombatMusic(_cosmeticSettings.DisableCombatMusic == CombatMusic.WeakEnemies);
         }
+        #endregion
 
         private void WritePlayerModel()
         {
