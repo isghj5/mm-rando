560d1e024827ad99e45673d6414b10243096d034
ZoeyZolotova <zoey.zolotova@gmail.com>
2021-12-26 02:20:56 +1100

    Fix unrandomized snowballs in snowhead still showing 3d models.

--- MMR.Randomizer/GameObjects/Item.cs
+++ MMR.Randomizer/GameObjects/Item.cs
@@ -3984,7 +3984,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("a quiver refill", "a bundle of projectiles")]
         [ShopText("Ammo for your bow.", isMultiple: true)]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x1F8), ItemPool(ItemCategory.Arrows, LocationCategory.SmallSnowballs), CollectableIndex(0x2E21)]
+        [GetItemIndex(0x1F8), ItemPool(ItemCategory.Arrows, LocationCategory.SmallSnowballs), CollectableIndex(0x2E21), CollectableIndex(0x3920)]
         CollectableSnowheadSmallSnowball1,
 
 
@@ -4994,7 +4994,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("explosives")]
         [ShopText("Explosives. You need a Bomb Bag to carry them.", isMultiple: true)]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x25D), ItemPool(ItemCategory.Bombs, LocationCategory.SmallSnowballs), CollectableIndex(0x2E20)]
+        [GetItemIndex(0x25D), ItemPool(ItemCategory.Bombs, LocationCategory.SmallSnowballs), CollectableIndex(0x2E20), CollectableIndex(0x3929)]
         CollectableSnowheadSmallSnowball2,
 
 
@@ -6394,7 +6394,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("a magic refill")]
         [ShopText("Replenishes a large amount of your magic power.")]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x2E9), ItemPool(ItemCategory.MagicJars, LocationCategory.SmallSnowballs), CollectableIndex(0x2E23)]
+        [GetItemIndex(0x2E9), ItemPool(ItemCategory.MagicJars, LocationCategory.SmallSnowballs), CollectableIndex(0x2E23), CollectableIndex(0x3922)]
         CollectableSnowheadSmallSnowball3,
 
 
@@ -7384,7 +7384,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("a magic refill")]
         [ShopText("Replenishes a small amount of your magic power.")]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x34C), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E26)]
+        [GetItemIndex(0x34C), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E26), CollectableIndex(0x3925)]
         CollectableSnowheadLargeSnowball3,
 
 
@@ -7404,7 +7404,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("a magic refill")]
         [ShopText("Replenishes a small amount of your magic power.")]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x34E), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E28)]
+        [GetItemIndex(0x34E), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E28), CollectableIndex(0x3927)]
         CollectableSnowheadLargeSnowball5,
 
 
@@ -7414,7 +7414,7 @@ namespace MMR.Randomizer.GameObjects
         [GossipLocationHint("a mountain-top snowball"), GossipItemHint("a magic refill")]
         [ShopText("Replenishes a small amount of your magic power.")]
         [ChestType(ChestTypeAttribute.ChestType.SmallWooden)]
-        [GetItemIndex(0x34F), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E29)]
+        [GetItemIndex(0x34F), ItemPool(ItemCategory.MagicJars, LocationCategory.LargeSnowballs), CollectableIndex(0x2E29), CollectableIndex(0x3928)]
         CollectableSnowheadLargeSnowball6,
 
 
