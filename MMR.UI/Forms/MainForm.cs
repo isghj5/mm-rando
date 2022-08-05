c51abd38c4173e0eb51483fbf8dac3b9085c6068
Isghj5 <isghj5@gmail.com>
2022-08-04 21:14:25 -0700

    Actorizer: version update

--- MMR.UI/Forms/MainForm.cs
+++ MMR.UI/Forms/MainForm.cs
@@ -72,7 +72,7 @@ namespace MMR.UI.Forms
             #if DEBUG
             Text = $"Majora's Mask Randomizer v{Randomizer.AssemblyVersion} + DEBUG ON";
             #else
-            Text = $"Majora's Mask Randomizer v{Randomizer.AssemblyVersion} + Isghj's Enemizer Test 35.4";
+            Text = $"Majora's Mask Randomizer v{Randomizer.AssemblyVersion} + Isghj's Enemizer Test 36.0";
             #endif
 
             var args = Environment.GetCommandLineArgs();
