2a58178bd065d9071128fafecfac61eafd8d49af
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-01-18 16:45:01 +1100

    Add command to set a log channel for the discord bot. Add a command usable in the log channel to kill a seed currently being generated.
    (cherry picked from commit 1d87b9ff17685d43e98ee6d08c61ee888983fe61)

---
+++ MMR.DiscordBot/Data/Repositories/LogChannelRepository.cs
@@ -0,0 +1,11 @@
+﻿using MMR.DiscordBot.Data.Entities;
+
+namespace MMR.DiscordBot.Data.Repositories
+{
+    public class LogChannelRepository : BaseRepository<LogChannelEntity>
+    {
+        public LogChannelRepository(ConnectionFactory connectionFactory) : base(connectionFactory)
+        {
+        }
+    }
+}
