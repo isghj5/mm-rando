26e01bc9c401746eb4cd69f8866ab9cfa3e230b1
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-01-22 12:38:02 +1100

    Game no longer unpauses when you input respawn button combo on epona.

--- assembly/c/Game.c
+++ assembly/c/Game.c
@@ -32,11 +32,11 @@ static void CheckRespawn(GlobalContext* ctxt) {
     switch (z2_GetMessageState(&ctxt->msgCtx)) {
         case 0: // TEXT_STATE_NONE
             if (curButtons.z && curButtons.r && curButtons.a && ctxt->state.input[0].pressEdge.buttons.s) {
+                ctxt->state.input[0].pressEdge.buttons.s = 0;
                 if (GET_PLAYER(ctxt)->stateFlags.state1 & PLAYER_STATE1_EPONA) {
                     z2_PlaySfx(0x4806); // NA_SE_SY_ERROR
                     return;
                 } else {
-                    ctxt->state.input[0].pressEdge.buttons.s = 0;
                     z2_ShowMessage(ctxt, respawnTextId, NULL);
                 }
             }
