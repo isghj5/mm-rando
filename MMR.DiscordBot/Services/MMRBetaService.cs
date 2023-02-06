e970eb6d282067973667f2a1392cba9edb8ce308
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-02-06 17:48:54 +1100

    Preparing discord bot for adding more versions.

--- MMR.DiscordBot/Services/MMRBetaService.cs
+++ MMR.DiscordBot/Services/MMRBetaService.cs
@@ -5,19 +5,6 @@ namespace MMR.DiscordBot.Services
 {
     public class MMRBetaService : MMRService
     {
-        private const string MMR_CLI = "MMRBETA_CLI";
-
-        public MMRBetaService() : base()
-        {
-            _cliPath = Environment.GetEnvironmentVariable(MMR_CLI);
-            if (string.IsNullOrWhiteSpace(_cliPath))
-            {
-                _cliPath = null;
-            }
-            else if (!Directory.Exists(_cliPath))
-            {
-                _cliPath = null;
-            }
-        }
+        protected override string MMR_CLI => "MMRBETA_CLI";
     }
 }
