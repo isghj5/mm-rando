0f5a163663da8969699bbdaa7b1f8367da8575a1
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-08-06 21:12:05 +1000

    Properly remove unnecessary big octo spit velocity code.

--- assembly/src/GiantMask.asm
+++ assembly/src/GiantMask.asm
@@ -16,17 +16,3 @@ GiantMask_GetLedgeWalkOffHeight_Hook:
     lw      ra, 0x001C (sp)
     jr      ra
     addiu   sp, sp, 0x20
-
-GiantMask_GetBigOctoSpitVelocity_Hook:
-    addiu   sp, sp, -0x18
-    sw      ra, 0x0014 (sp)
-
-    jal     GiantMask_GetBigOctoSpitVelocity
-    swc1    f0, 0x0010 (sp)
-
-    mov.s   f2, f0
-
-    lwc1    f0, 0x0010 (sp)
-    lw      ra, 0x0014 (sp)
-    jr      ra
-    addiu   sp, sp, 0x18
