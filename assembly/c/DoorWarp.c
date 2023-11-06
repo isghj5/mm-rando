9c0e965642f0966b93c9e269749405a66cf1f7ce
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-11-06 22:50:05 +1100

    A boss warp without an item in it can now trigger the giants cutscene.

--- assembly/c/DoorWarp.c
+++ assembly/c/DoorWarp.c
@@ -2,6 +2,36 @@
 #include "Misc.h"
 #include "MMR.h"
 
+static void SetCutscene(ActorDoorWarp1* actor) {
+    u8 count = 0;
+    if (gSaveContext.perm.inv.questStatus.odolwasRemains) {
+        count++;
+    }
+    if (gSaveContext.perm.inv.questStatus.gohtsRemains) {
+        count++;
+    }
+    if (gSaveContext.perm.inv.questStatus.gyorgsRemains) {
+        count++;
+    }
+    if (gSaveContext.perm.inv.questStatus.twinmoldsRemains) {
+        count++;
+    }
+
+    if (MISC_CONFIG.speedups.skipGiantsCutscene) {
+        count = MISC_CONFIG.speedups.oathHint && !gSaveContext.perm.inv.questStatus.oathToOrder && count >= MISC_CONFIG.MMRbytes.requiredBossRemains ? 4 : 0;
+    } else if (count >= MISC_CONFIG.MMRbytes.requiredBossRemains) {
+        count = 4;
+    }
+
+    u8 oldCount = gSaveContext.perm.unk_ECC[1] & 0xFF;
+    if (count > oldCount) {
+        actor->unk_202 = count;
+        gSaveContext.perm.unk_ECC[1] = (gSaveContext.perm.unk_ECC[1] & 0xFFFFFF00) | count;
+    } else {
+        actor->unk_202 = 0;
+    }
+}
+
 void DoorWarp_GiveItem(ActorDoorWarp1* actor, GlobalContext* ctxt) {
     actor->warpTimer--;
     if (actor->warpTimer == 0) {
@@ -9,39 +39,17 @@ void DoorWarp_GiveItem(ActorDoorWarp1* actor, GlobalContext* ctxt) {
             MMR_ProcessItem(ctxt, 0x77, false);
         }
 
-        u8 count = 0;
-        if (gSaveContext.perm.inv.questStatus.odolwasRemains) {
-            count++;
-        }
-        if (gSaveContext.perm.inv.questStatus.gohtsRemains) {
-            count++;
-        }
-        if (gSaveContext.perm.inv.questStatus.gyorgsRemains) {
-            count++;
-        }
-        if (gSaveContext.perm.inv.questStatus.twinmoldsRemains) {
-            count++;
-        }
-
-        if (MISC_CONFIG.speedups.skipGiantsCutscene) {
-            count = MISC_CONFIG.speedups.oathHint && !gSaveContext.perm.inv.questStatus.oathToOrder && count >= MISC_CONFIG.MMRbytes.requiredBossRemains ? 4 : 0;
-        } else if (count >= MISC_CONFIG.MMRbytes.requiredBossRemains) {
-            count = 4;
-        }
-
-        u8 oldCount = gSaveContext.perm.unk_ECC[1] & 0xFF;
-        if (count > oldCount) {
-            actor->unk_202 = count;
-            gSaveContext.perm.unk_ECC[1] = (gSaveContext.perm.unk_ECC[1] & 0xFFFFFF00) | count;
-        } else {
-            actor->unk_202 = 0;
-        }
+        SetCutscene(actor);
     }
 }
 
 void DoorWarp_GiveItem2(ActorDoorWarp1* actor, GlobalContext* ctxt) {
-    if (ctxt->interfaceCtx.restrictionFlags[4] && actor->warpTimer2 == 0x90 && !MISC_CONFIG.internal.vanillaLayout && !MMR_GetGiFlag(0x77)) {
-        MMR_ProcessItem(ctxt, 0x77, false);
+    if (ctxt->interfaceCtx.restrictionFlags[4] && actor->warpTimer2 == 0x90 && !MISC_CONFIG.internal.vanillaLayout) {
+        if (!MMR_GetGiFlag(0x77)) {
+            MMR_ProcessItem(ctxt, 0x77, false);
+        }
+
+        SetCutscene(actor);
     }
 }
 
