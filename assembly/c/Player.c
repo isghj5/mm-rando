d63a316121aa18db4dc87cc1954d72b7c95fb4aa
ZoeyZolotova <zoey.zolotova@gmail.com>
2023-11-09 18:47:50 +1100

    Remove check for AIR state which may or may not be redundant, but it caused the big octo to be removed as the player's parent which caused it to consume all the deku hops.

--- assembly/c/Player.c
+++ assembly/c/Player.c
@@ -69,8 +69,7 @@ void Player_PreventDangerousStates(ActorPlayer* player) {
     // parent can be hookshot or epona
     // "&& player->base.parent->id == ACTOR_ARMS_HOOK" doesn't work because parent might be stale reference
     if (player->base.parent) {
-        if ((player->stateFlags.state1 & PLAYER_STATE1_AIR) // might be redundant with the sPlayer_Falling check below
-            || (player->stateFlags.state3 & PLAYER_STATE3_JUMP_ATTACK)
+        if ((player->stateFlags.state3 & PLAYER_STATE3_JUMP_ATTACK)
             || (player->actionFunc == sPlayer_Falling
                 && !(player->stateFlags.state3 & PLAYER_STATE3_HOOK_ARRIVE_2))) {
             player->base.parent = NULL;
