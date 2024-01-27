a731756a34597148fb1008cf6ff18c9adcf7b4f5
ZoeyZolotova <zoey.zolotova@gmail.com>
2024-01-27 17:43:21 +1100

    Prevent opening doors when giant.

--- assembly/c/DoorShutter.c
+++ assembly/c/DoorShutter.c
@@ -1,6 +1,10 @@
 #include <z64.h>
+#include "GiantMask.h"
 
 f32 DoorShutter_GetMaxZDistToOpen(ActorPlayer* player) {
+    if (GiantMask_IsGiant()) {
+        return 0.0f;
+    }
     f32 zDist = 50.0f;
     if (player->form == PLAYER_FORM_FIERCE_DEITY) { // TODO && FDAnywhere setting is enabled
         zDist = 63.0f;
