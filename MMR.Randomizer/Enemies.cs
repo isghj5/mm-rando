fa4387b4247637a34229e649536c94529437b7c1
isghj8 <isghj8@gmail.com>
2024-02-03 20:35:04 -0800

    Enemizer: block followingDinofos actor until I can fix it

--- MMR.Randomizer/Enemies.cs
+++ MMR.Randomizer/Enemies.cs
@@ -3932,7 +3932,8 @@ namespace MMR.Randomizer
 
             foreach (string filePath in GenerateMMRAFileList(directory))
             {
-                if (filePath.Contains("SafeBoat.mmra"))
+                if (filePath.Contains("SafeBoat.mmra") ||
+                    filePath.Contains("FollowingDinofos.mmra"))
                     //throw new Exception("SafeBoat.mmra no longer works in actorizer 1.16, \n remove the file from MMR/actors and start a new seed.");
                     continue;
 
