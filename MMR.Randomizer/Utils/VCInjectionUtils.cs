54843a4f4ca098f603d9408be9ca88162189648b
Rebbacus <100238547+Rebbacus@users.noreply.github.com>
2024-02-05 00:28:00 -0500

    Increase WiiVC ROM limit and have it work this time.

--- MMR.Randomizer/Utils/VCInjectionUtils.cs
+++ MMR.Randomizer/Utils/VCInjectionUtils.cs
@@ -28,19 +28,51 @@ namespace MMR.Randomizer.Utils
 
         private static void GetApp5(byte[] ROM, string VCDir)
         {
-            BinaryReader a50 = new BinaryReader(File.OpenRead(Path.Combine(VCDir, "5-0")));
             BinaryReader a51 = new BinaryReader(File.OpenRead(Path.Combine(VCDir, "5-1")));
             BinaryWriter app5 = new BinaryWriter(File.Open(Path.Combine(VCDir, "00000005.app"), FileMode.Create));
-            byte[] buffer = new byte[a50.BaseStream.Length];
-            a50.Read(buffer, 0, buffer.Length);
+            byte[] buffer = VCReadA50(ROM.Length - 4, VCDir);
             app5.Write(buffer);
             app5.Write(ROM);
             buffer = new byte[a51.BaseStream.Length];
             a51.Read(buffer, 0, buffer.Length);
             app5.Write(buffer);
-            a50.Close();
             a51.Close();
             app5.Close();
+        }
+
+        private static byte[] VCReadA50(int romsize, string VCDir)
+        {
+            BinaryReader a50 = new BinaryReader(File.OpenRead(Path.Combine(VCDir, "5-0")));
+            byte[] buffer = new byte[a50.BaseStream.Length];
+            a50.Read(buffer, 0, buffer.Length);
+
+            // If over 32MB, rom size and file offsets in the included "5-0" u8 archive chunk need to be updated
+            if (romsize > 0x02000000)
+            {
+                buffer[0x574] = (byte)(romsize >> 24 & 0xFF);
+                buffer[0x575] = (byte)(romsize >> 16 & 0xFF);
+                buffer[0x576] = (byte)(romsize >> 8 & 0xFF);
+                buffer[0x577] = (byte)((romsize & 0xFF) + 4);
+
+                int entryoffset = 0x0578;
+                for (int i = 0; i < 0x4C; i++)
+                {
+                    if ((buffer[entryoffset] & 0x01) == 0x00)
+                    {
+                        int fileoffset = ((buffer[entryoffset + 4] << 24) + (buffer[entryoffset + 5] << 16) + (buffer[entryoffset + 6] << 8) + buffer[entryoffset + 7]
+                                           + (romsize - 0x02000000));
+
+                        buffer[entryoffset + 4] = (byte)(fileoffset >> 24 & 0xFF);
+                        buffer[entryoffset + 5] = (byte)(fileoffset >> 16 & 0xFF);
+                        buffer[entryoffset + 6] = (byte)(fileoffset >> 8 & 0xFF);
+                        buffer[entryoffset + 7] = (byte)(fileoffset & 0xFF);
+                    }
+                    entryoffset += 0xC;
+                }
+            }
+
+            a50.Close();
+            return buffer.ToArray();
         }
 
         private static void DeleteApp5(string VCDir)
@@ -50,7 +82,17 @@ namespace MMR.Randomizer.Utils
 
         private static byte[] AddVCHeader(byte[] ROM)
         {
-            byte[] Header = new byte[] { 0x08, 0x00, 0x00, 0x00 };
+            // This header dictates how much space in wii memory to allocate for the ROM
+            byte[] Header;
+            if (ROM.Length > 0x02000000)
+            {
+                int headersize = ROM.Length << 2;
+                Header = new byte[] { (byte)(headersize >> 24 & 0xFF), (byte)(headersize >> 16 & 0xFF), (byte)(headersize >> 8 & 0xFF), (byte)(headersize & 0xFF) };
+            } else
+            {
+                Header = new byte[] { 0x08, 0x00, 0x00, 0x00 };
+            }
+            
             return Header.Concat(ROM).ToArray();
         }
 
