3b436fd2716879cb7ba6c66625e2cc235288cd19
isghj8 <isghj8@gmail.com>
2024-04-15 20:36:27 -0700

    Actorizer: added missing buisness scrub ikana gossip stone fairy check

--- MMR.Randomizer/GameObjects/Actor.cs
+++ MMR.Randomizer/GameObjects/Actor.cs
@@ -5533,7 +5533,7 @@ namespace MMR.Randomizer.GameObjects
         [CheckRestricted(Scene.ZoraHallRooms, variant: -1,
             Item.ShopItemBusinessScrubGreenPotion, Item.TradeItemOceanDeed, Item.HeartPieceZoraHallScrub, Item.ShopItemBusinessScrubBluePotion)]
         [CheckRestricted(Scene.IkanaCanyon, variant: -1,
-            Item.ShopItemBusinessScrubBluePotion, Item.IkanaScrubGoldRupee, Item.HeartPieceIkana /* no scrub goes back to here */)]
+            Item.ShopItemBusinessScrubBluePotion, Item.IkanaScrubGoldRupee, Item.HeartPieceIkana, Item.CollectableIkanaCanyonSakonSHideoutAreaGossipFairy1)]
         // 0xFC08, 0x1000 are clear swamp
         //0x4 is a flag, meaning the actor has a path, checks if 0xFC00 is a path or not and self terminates
         //[GroundVariants(0xFC08, 0x1000, 0xFC04, 0xFC07, 0x1001, 0x0402, 0xFC06, 0x0001, 0x1800, 0x1003)]
