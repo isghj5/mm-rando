5ef4fb938b5d2a674bb73380eaf8af31f92688de
isghj8 <isghj8@gmail.com>
2024-05-12 22:55:23 -0700

    Actorizer: mark ceiling gold skulltula to avoid hard to reach skull token in dungeons

--- MMR.Randomizer/GameObjects/Actor.cs
+++ MMR.Randomizer/GameObjects/Actor.cs
@@ -822,6 +822,8 @@ namespace MMR.Randomizer.GameObjects
         // we want to put them on the ceiling, possibly
         [CeilingVariants(0xFF53, 0xFF07, 0xFF56, 0xFF62, 0xFF76, 0xFF03,
             0xFF3F, 0xFF3B, 0xFF5D, 0xFF61, 0xFF6D, 0xFF0B, 0xFF0F, 0xFFFC)] // WARNING: right now we do not remove them from their old place, but if we did this would put ceiling actors on the wall
+        [RespawningVariants(0xFF53, 0xFF07, 0xFF56, 0xFF62, 0xFF76, 0xFF03,
+            0xFF3F, 0xFF3B, 0xFF5D, 0xFF61, 0xFF6D, 0xFF0B, 0xFF0F, 0xFFFC)] // mark all ceiling types as respawning to avoid being placed on kill skulltula to get skull token enemies
         [ForbidFromScene(Scene.SwampSpiderHouse, Scene.OceanSpiderHouse)] // dont remove old spiders, the new ones might not be gettable
         [TreasureFlagsPlacement(mask: 0x3F, shift: 2)] // for some reason it collides with path sometimes, TODO figure this out
         GoldSkulltula = 0x50, // En_Sw "Skullwalltulla"
