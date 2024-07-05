29c3fbce45638aae966eb966765a002da197e093
isghj8 <isghj8@gmail.com>
2024-07-04 22:08:15 -0800

    Version distinction for zfg camera

--- MMR.Randomizer/Utils/SpoilerUtils.cs
+++ MMR.Randomizer/Utils/SpoilerUtils.cs
@@ -72,7 +72,7 @@ namespace MMR.Randomizer.Utils
             var plainTextRegex = new Regex("[^a-zA-Z0-9' .\\-]+");
             Spoiler spoiler = new Spoiler()
             {
-                Version = Randomizer.AssemblyVersion + " + Isghj's Enemizer Test 71.0",
+                Version = Randomizer.AssemblyVersion + " + Isghj's Enemizer Test 71.0 + ZFG Camera",
                 SettingsString = settingsString,
                 Seed = randomized.Seed,
                 DungeonEntrances = dungeonEntrances,
