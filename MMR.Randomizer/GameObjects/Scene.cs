2ccd6eb5d183baec6428c7dfaa241174680709a4
isghj8 <isghj8@gmail.com>
2024-08-19 13:17:56 -0800

    Actorizer: mark mountain spring bronze boulders as blocking, replacements can block the door

--- MMR.Randomizer/GameObjects/Scene.cs
+++ MMR.Randomizer/GameObjects/Scene.cs
@@ -768,6 +768,10 @@ namespace MMR.Randomizer.GameObjects
         [DynaHeadroom(64, 64)]  // low default to start
         [EnemizerSceneEnemyReplacementBlock(Actor.HoneyComb,
             Actor.UnusedStoneTowerPlatform, Actor.UnusedStoneTowerStoneElevator)] // until I change the combs to be wall or flying only this is weird
+        [EnemizerSceneEnemyReplacementBlock(Actor.BronzeBoulder,
+            Actor.UnusedStoneTowerPlatform, Actor.UnusedStoneTowerStoneElevator,
+            Actor.RegularIceBlock, Actor.ClocktowerGearsAndOrgan, Actor.Bumper, Actor.Bombiwa, Actor.PushableBlock, Actor.Bg_Heavy_Block,
+            Actor.LostWoodsCutsceneTrees, Actor.Treee)] // tall blocks door
         MountainVillageSpring = 0x57,
 
         [FileID(1449)]
