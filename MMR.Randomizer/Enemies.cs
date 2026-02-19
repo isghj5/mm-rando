77e351a8090ca95f2dadf56acad06c6a5db1dcd2
isghj8 <isghj8@gmail.com>
2026-02-19 13:32:47 -0800

    Actorizer: version update for first winforms test build

--- MMR.Randomizer/Enemies.cs
+++ MMR.Randomizer/Enemies.cs
@@ -7974,7 +7974,7 @@ namespace MMR.Randomizer
                     sw.WriteLine(""); // spacer from last flush
                     sw.WriteLine("Enemizer final completion time: " + ((DateTime.Now).Subtract(enemizerStartTime).TotalMilliseconds).ToString() + "ms ");
                     sw.Write(_syncedLog.ToString());
-                    sw.Write("Enemizer version: Isghj's Actorizer Test 96.1\n");
+                    sw.Write("Enemizer version: Isghj's Actorizer Test 97.0\n");
                     sw.Write("seed: [ " + seed + " ]");
                 }
             }
