0bde96a63155186d94139add0ea041e0647d1333
isghj8 <isghj8@gmail.com>
2026-05-13 14:14:57 -0800

    actorizer; somehow I forgot to set post correctly

--- MMR.Randomizer/Enemies.cs
+++ MMR.Randomizer/Enemies.cs
@@ -3417,7 +3417,7 @@ namespace MMR.Randomizer
             thisSceneData.Log.Append(bogoLog);
             WriteOutput("####################################################### ");
 
-            CheckForHardToFindBugsPre(thisSceneData);
+            CheckForHardToFindBugsPost(thisSceneData);
 
             // realign all scene companion actors
             MoveAlignedCompanionActors(thisSceneData);
