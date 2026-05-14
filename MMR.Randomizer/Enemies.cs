ded94098ca283648f739177be25d27940ae849c2
isghj8 <isghj8@gmail.com>
2026-05-14 13:06:06 -0800

    Actorizer: version update

--- MMR.Randomizer/Enemies.cs
+++ MMR.Randomizer/Enemies.cs
@@ -3526,7 +3526,7 @@ namespace MMR.Randomizer
                     sw.WriteLine(""); // spacer from last flush
                     sw.WriteLine("Enemizer final completion time: " + ((DateTime.Now).Subtract(enemizerStartTime).TotalMilliseconds).ToString() + "ms ");
                     sw.Write(_syncedLog.ToString());
-                    sw.Write("Enemizer version: Isghj's Actorizer Test 97.3\n");
+                    sw.Write("Enemizer version: Isghj's Actorizer Test 99.0\n");
                     sw.Write("seed: [ " + seed + " ]");
                 }
             }
